bea09a5318081ffa302b89e217d99b4e5e18f480
Zofia Grodecka <zgrodecka@gmail.com>
2024-03-05 16:56:02 +0100

    Add suspense and streaming

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode } from "react";
+import { Suspense, type ReactNode } from "react";
 import { ProductsList } from "./productsList";
 
 export default async function ProductsLayout({
@@ -10,9 +10,11 @@ export default async function ProductsLayout({
     <div className="mx-auto grid min-h-screen w-full max-w-7xl grid-cols-12 gap-x-8 bg-white">
       <aside className="col-span-3 px-8 py-4 shadow-xl">
         <h2 className="mb-4 text-xl font-bold">Polecane produkty</h2>
-        <ul>
-          <ProductsList page={3} />
-        </ul>
+        <Suspense>
+          <ul>
+            <ProductsList page={3} />
+          </ul>
+        </Suspense>
       </aside>
       <main className="col-span-9 px-8 py-4 shadow-xl">{children}</main>
     </div>
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,18 @@
+import { Suspense } from "react";
 import { ProductsList } from "./productsList";
 
 export default async function ProductsPage() {
   return (
     <div>
       <h2 className="mb-4 text-xl font-bold">Lista produktów</h2>
+
       <ul>
-        <ProductsList page={1} />
-        <ProductsList page={2} />
+        <Suspense>
+          <ProductsList page={1} />
+        </Suspense>
+        <Suspense>
+          <ProductsList page={2} />
+        </Suspense>
       </ul>
     </div>
   );
