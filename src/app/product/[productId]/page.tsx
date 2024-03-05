2ff5d6bab33c8f47ed8145a7347817be80b6e9cf
Zofia Grodecka <zgrodecka@gmail.com>
2024-03-05 17:08:56 +0100

    Add generateStaticParams

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,3 +1,10 @@
+export async function generateStaticParams() {
+  const res = await fetch(`https://naszsklep-api.vercel.app/api/products`);
+  const products = (await res.json()) as { id: string; title: string }[];
+
+  return products.map((product) => ({ productId: product.id }));
+}
+
 export default async function SingleProductPage({
   params,
 }: {
